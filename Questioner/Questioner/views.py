8dce69a8dd54a1ef50ce0bb814c7d759c3ff202e
amit mataghare <amit.mataghare@gmail.com>
2017-09-07 12:11:17 +0530

    display question and answer with resp to their user

---
+++ Questioner/Questioner/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render_to_response, render
+from models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseRedirect, HttpResponse, HttpRequest, JsonResponse
+
+
+
+def get_questions(request):
+    data = []
+    for que in Question.objects.all():
+        obj = {
+            "title": que.title,
+            "id": que.id,
+            "answers": [{
+                "answer": ans.body,
+                "user": ans.user.name,
+                "id": ans.id
+            } for ans in que.answer_set.all()]
+        }
+        data.append(obj)
+    return JsonResponse({
+        "data": data, 
+        "status": True,
+    })
+
